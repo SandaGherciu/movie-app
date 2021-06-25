90c3ca425a37762355de1447c86e88b019f589a3
Sanda <sandagherciu@gmail.com>
2021-06-25 18:03:29 +0300

    Added small modifications

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import "./App.scss";
 import React, { useState, useEffect } from "react";
+const MOVIE_DB_API_URL = "https://api.themoviedb.org/3";
 
 function App() {
   const [input, setInput] = useState("");
   const [search, setSearch] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [movies, setMovies] = useState([]);
-  const apiUrl =
-    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+  const apiUrl = `${MOVIE_DB_API_URL}/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1`;
   const imgUrl = "https://image.tmdb.org/t/p/original";
+
   const handleChange = (event) => {
     setInput(event.target.value);
     console.log(search);
   };
+
   const handleSearch = async () => {
     try {
       const result = await fetch(
-        `https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=${input}&append_to_response=images`
+        `${MOVIE_DB_API_URL}/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=${input}&append_to_response=images`
       );
       if (result.ok) {
         const searchResults = await result.json();
@@ -51,22 +53,26 @@ function App() {
     <div className="App">
       <header>
         <h1>Movies</h1>
+
         <div is="search-bar">
           <input
             type="search"
             value={input}
             placeholder="Search..."
             onChange={handleChange}
-          ></input>
+          />
           <button onClick={handleSearch}>Search</button>
         </div>
       </header>
+
       <div id="container">
         <p id="search-text">{search}</p>
+
         <div id="search-results">
           {movies.map((movie) => {
             const path =
               movie.poster_path || movie.backdrop_path || movie.still_path;
+
             return (
               <div key={movie.id} className="movies">
                 <img
@@ -76,6 +82,7 @@ function App() {
                   width="180px"
                   height="auto"
                 />
+
                 <div className="titles">
                   <p>{movie.title}</p>
                 </div>
